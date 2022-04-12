fca14298746fa52c56114cb4eb6d6a3c8709424b
Druids Mickey Jo <interskysolutions@gmail.com>
2022-04-12 23:06:57 +0400

    4th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
-  {path:'aboutus', component: AboutusComponent}
+  {path:'aboutus', component: AboutusComponent},
+  {path:'header', component: HeaderComponent},
+  {path:'footer', component: FooterComponent}
 ];
 
 @NgModule({
