492d50534f9345f00da07a9815b653ee399f8519
DruidsMickey <michaellam@intersky.mu>
2022-05-19 15:59:54 +0400

    ajax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
-import { ContactusComponent} from './contactus/contactus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AjaxComponent } from './ajax/ajax.component';
 
 const routes: Routes = [
   {path:'', component: MainComponent},
   {path:'aboutus', component: AboutusComponent},
   {path:'header', component: HeaderComponent},
   {path:'footer', component: FooterComponent},
-   {path:'contactus', component: ContactusComponent} 
+  {path:'ajax', component: AjaxComponent},
+  {path:'contactus', component: ContactusComponent} 
 ];
 
 @NgModule({
